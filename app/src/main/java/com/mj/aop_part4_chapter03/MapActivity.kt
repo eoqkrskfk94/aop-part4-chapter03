e7b3fc2afb6ca37c3db58034f3b6e9cd383cc1d0
MJ <eoqkrskfk94@naver.com>
2021-06-28 00:04:08 +0900

    [Add] 내 위치 불러오기

--- app/src/main/java/com/mj/aop_part4_chapter03/MapActivity.kt
+++ app/src/main/java/com/mj/aop_part4_chapter03/MapActivity.kt
@@ -1,7 +1,16 @@
 package com.mj.aop_part4_chapter03
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,18 +19,30 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.mj.aop_part4_chapter03.databinding.ActivityMapBinding
+import com.mj.aop_part4_chapter03.model.LocationLatLngEntity
 import com.mj.aop_part4_chapter03.model.SearchResultEntity
+import com.mj.aop_part4_chapter03.utility.RetrofitUtil
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     private lateinit var binding: ActivityMapBinding
     private lateinit var searchResult: SearchResultEntity
     private lateinit var map: GoogleMap
     private var currentSelectMarker: Marker? = null
+    private lateinit var locationManager: LocationManager
+    private lateinit var myLocationListener: MyLocationListener
 
-    companion object{
+    companion object {
         const val SEARCH_RESULT_EXTRA_KEY = "SEARCH_RESULT_EXTRA_KEY"
         const val CAMERA_ZOOM_LEVEL = 17f
+        const val PERMISSION_REQUEST_CODE = 100
     }
 
 
@@ -31,9 +52,10 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if(::searchResult.isInitialized.not()){
+        if (::searchResult.isInitialized.not()) {
             intent?.let {
-                searchResult = it.getParcelableExtra<SearchResultEntity>(SEARCH_RESULT_EXTRA_KEY) ?: throw Exception("no data")
+                searchResult = it.getParcelableExtra<SearchResultEntity>(SEARCH_RESULT_EXTRA_KEY)
+                        ?: throw Exception("no data")
                 setupGoogleMap()
             }
         }
@@ -45,6 +67,114 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun bindViews() = with(binding) {
         currentLocationButton.setOnClickListener {
             getMyLocation()
+        }
+    }
+
+    private fun getMyLocation() {
+        if (::locationManager.isInitialized.not()) {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        if (isGpsEnabled) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)
+            } else {
+                setMyLocationListener()
+            }
+
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setMyLocationListener() {
+        val minTime = 1500L
+        val minDistance = 100f
+
+        if (::myLocationListener.isInitialized.not()) {
+            myLocationListener = MyLocationListener()
+        }
+        with(locationManager) {
+            requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime, minDistance, myLocationListener
+            )
+        }
+    }
+
+    private fun onCurrentLocationChanged(locationLatLngEntity: LocationLatLngEntity) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(
+                locationLatLngEntity.latitude.toDouble(),
+                locationLatLngEntity.longitude.toDouble()
+        ), CAMERA_ZOOM_LEVEL))
+
+        localReverseGeoInformation(locationLatLngEntity)
+        removeLocationListener()
+    }
+
+    private fun localReverseGeoInformation(locationLatLngEntity: LocationLatLngEntity) {
+        launch(coroutineContext) {
+            try {
+                withContext(Dispatchers.IO) {
+                    val response = RetrofitUtil.apiService.getReverseGeoCode(
+                            lat = locationLatLngEntity.latitude.toDouble(),
+                            lon = locationLatLngEntity.longitude.toDouble()
+                    )
+
+                    if (response.isSuccessful) {
+                        val body = response.body()
+                        withContext(Dispatchers.Main) {
+                            body?.let {
+                                val marker = setupMarker(SearchResultEntity(
+                                        fullAddress = it.addressInfo.fullAddress ?: "no address",
+                                        name = "my location",
+                                        locationLatLng = locationLatLngEntity
+                                ))
+                                currentSelectMarker?.showInfoWindow()
+                            }
+                        }
+                    }
+
+
+                }
+            } catch (e: java.lang.Exception) {
+
+            }
+        }
+    }
+
+    private fun removeLocationListener() {
+        if (::locationManager.isInitialized && ::myLocationListener.isInitialized) {
+            locationManager.removeUpdates(myLocationListener)
+        }
+    }
+
+    inner class MyLocationListener : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val locationLatLngEntity = LocationLatLngEntity(
+                    location.latitude.toFloat(),
+                    location.longitude.toFloat()
+            )
+
+            onCurrentLocationChanged(locationLatLngEntity)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                setMyLocationListener()
+            } else {
+
+            }
         }
     }
 
@@ -61,8 +191,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun setupMarker(searchResultEntity: SearchResultEntity): Marker {
         val positionLatLng = LatLng(
-            searchResultEntity.locationLatLng.latitude.toDouble(),
-            searchResultEntity.locationLatLng.longitude.toDouble())
+                searchResultEntity.locationLatLng.latitude.toDouble(),
+                searchResultEntity.locationLatLng.longitude.toDouble())
 
         val markerOptions = MarkerOptions().apply {
             position(positionLatLng)
